0b933822dd1e69d8d5281b7dff09a5d2e0121edd
Tsut-ps <73014392+Tsut-ps@users.noreply.github.com>
2025-12-29 02:46:22 +0900

    feat: フッター＞スタイルを微調整

--- app/components/FooterSection.vue
+++ app/components/FooterSection.vue
@@ -1,17 +1,15 @@
 <template>
   <footer class="footer-section">
-    <p>© Tsut-ps.</p>
-    <p>
-      Deployed with <a href="https://github.com/Tsut-ps/about" target="_blank">GitHub</a>.
-    </p>
+    <p>© Tsut-ps. Deployed with <a href="https://github.com/Tsut-ps/about" target="_blank">GitHub</a>.</p>
   </footer>
 </template>
 
 <style scoped>
 .footer-section {
-  padding: 2rem 1rem 3rem;
+  padding: 3rem 1rem;
   color: var(--color-text);
   text-align: center;
   font-size: 0.85rem;
+  opacity: 0.35;
 }
 </style>
